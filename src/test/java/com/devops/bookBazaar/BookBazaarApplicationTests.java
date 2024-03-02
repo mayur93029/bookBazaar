089b9445fb53f1bc52455e38d3a3b299d53fbbd7
Jitendra Kumar Shah <jitendra.kumar.shah@sap.com>
2024-03-02 12:31:04 +0530

    Spring initilization

---
+++ src/test/java/com/devops/bookBazaar/BookBazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.bookBazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
